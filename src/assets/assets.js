25d881cafefbe7085fc016c7eed770db113a88da
Nishant-Agarwal7088 <anishant415@gmail.com>
2024-10-24 05:34:06 +0530

    Initial commit

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -10,7 +10,16 @@ import image_2 from './image_2.png'
 import image_3 from './image_3.png'
 import image_4 from './image_4.png'
 import image_5 from './image_5.png'
-
+import shoe_1 from './shoe_1.png'
+import shoe_2 from './shoe_2.png'
+import shoe_3 from './shoe_3.png'
+import shoe_1_vertical from './shoe_1_vertical.png'
+import shoe_2_vertical from './shoe_2_vertical.png'
+import shoe_3_vertical from './shoe_3_vertical.png'
+import shoe_4 from './shoe_4.png'
+import shoe_5 from './shoe_5.png'
+import shoe_4_vertical from './shoe_4_vertical.png'
+import shoe_5_vertical from './shoe_5_vertical.png'
 
 
 export const assets = {
@@ -26,6 +35,16 @@ export const assets = {
     image_3,
     image_4,
     image_5,
+    shoe_1,
+    shoe_2,
+    shoe_3,
+    shoe_1_vertical,
+    shoe_2_vertical,
+    shoe_3_vertical,
+    shoe_4,
+    shoe_5,
+    shoe_4_vertical,
+    shoe_5_vertical
  
 }
 
